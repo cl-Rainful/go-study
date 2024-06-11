c9ae6cab915ad4c54e96a5bc62d8b1462fc46be7
cl-Rainful <1690030129@qq.com>
2024-06-11 18:53:22 +0800

    下划线

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"rainful.link/hello"
+)
 
 func main() {
-	fmt.Print("hello world")
-	print("hello world")
+	// fmt.Print("hello world")
+	// print("hello world")
+
+	// hello.Print()
+
+	hello.Print()
+
+	// f, err := os.Open("./hello/hello.go")
+	// f.Close()
+	// fmt.Printf("err: %v\n", err)
+
+	f, _ := os.Open("./hello/hello.go")
+	fmt.Println(f.Name())
 }
